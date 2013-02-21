6ef8c12768858d7d7edcfa4eca987477a59931a4
Richard Northover <richard@northover.info>
2013-02-21 20:41:13 +0000

    improving crumbs

--- inc/core/template_parts.php
+++ inc/core/template_parts.php
@@ -18,16 +18,25 @@ $header = str_replace("%%pages_list_link%%", $pages_list_link, $header);
 
 if($page != "index"){
 
-	//$p = explode("/", $page); // then loop through to get breadcrumbs
+	$p = explode("/", $page); // then loop through to get breadcrumbs
 
-	$crumb = str_replace("/index", "", $page);
-	
-	if($crumb != "" && $crumb != "/"){
-		$header = str_replace("%%info_pagetitle%%", " > [".ucfirst($crumb)."](".$page.")", $header);
-		$start = str_replace("</title>", " > ".ucfirst($crumb)."</title>", $start);
-	} else {
-		$header = str_replace("%%info_pagetitle%%", "", $header);
+
+	foreach ($p as $n => $crumb) {
+
+		if($n==0){ $crumblink = $infopath.$p[0]; }
+		if($n==1){ $crumblink = $infopath.$p[0]."/".$p[1]; }
+		if($n==2){ $crumblink = $infopath.$p[0]."/".$p[1]."/".$p[2]; }
+		if($n==3){ $crumblink = $infopath.$p[0]."/".$p[1]."/".$p[2]."/".$p[3]; }
+
+		if($crumb !="index"){
+			$trail .= "> [".ucfirst($crumb)."](".$crumblink.")";
+			$titletrail .= " - ".ucfirst($crumb);
+		}
 	}
+
+	$header = str_replace("%%info_pagetitle%%", $trail, $header);
+	$start = str_replace("</title>", $titletrail, $start);
+
 
 } else {
 	$header = str_replace("%%info_pagetitle%%", "", $header);	
